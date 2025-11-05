4e2a017476df3cc44a4d999a974ab66a4ceba86a
Adriano FRANCI <adriano.franci@l-acoustics.com>
2025-11-05 10:38:43 +0100

    progress day 07

--- 2024/07/main_01.py
+++ 2024/07/main_01.py
@@ -1,4 +1,21 @@
 
+
+def calc(curr_result: int, next_digit_index: int, target_result: int, digits: list[int])-> bool:
+    if curr_result == target_result:
+        return True
+
+    if curr_result > target_result or next_digit_index > len(digits)-1:
+        return False
+
+    inter_result = curr_result + digits[next_digit_index]
+    res = calc(inter_result, next_digit_index+1, target_result, digits)
+    if not res:
+        inter_result = curr_result * digits[next_digit_index]
+        return calc(inter_result, next_digit_index+1, target_result, digits)
+    else:
+        return True
+
+
 def main():
     input_fn = "input_ex.txt"
     equations = []
@@ -10,10 +27,9 @@ def main():
             digits = [int(n) for n in initial_split[1].split()]
             equations.append((result, digits))
 
-    equations = iter(equations)
-    current_equation = next(equations)
-    equation_result = current_equation[0]
-    equation_digits = current_equation[1]
+    for equation in equations:
+        res = calc(0, 0, equation[0], equation[1])
+        print(f"{res} -> {equation[0]}: {equation[1]}")
 
 
 main()
